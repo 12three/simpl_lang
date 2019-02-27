46cb3378ac4d54a63f8576f81220aa5657323af6
12three <zakharenko.main@gmail.com>
2019-02-27 11:06:06 +0200

    create parser

--- index.js
+++ index.js
@@ -6,7 +6,36 @@ function skipSpace(string) {
     }
 
     return string.slice(first);
-};
+}
+
+function parseApply(expr, program) {
+    program = skipSpace(program);
+
+    if (program[0] !== '(') {
+        // isn`t a statement
+        return {
+            expr,
+            rest: program,
+         }
+    }
+
+
+    program = skipSpace(program.slice(1));
+    expr = { type: 'apply', operator: expr, args: [] };
+    while (program[0] !== ')') {
+        let arg = parseExpression(program);
+
+        expr.args.push(arg.expr);
+        program = skipSpace(arg.rest);
+        if (program[0] !== ',') {
+            program = skipSpace(program.slice(1));
+        } else if (program[0] !== ')') {
+            throw new SyntaxError(`Expected ',' or ')'`)
+        }
+    }
+
+    return parseApply(expr, program.slice(1))
+}
 
 function parseExpression(program) {
     program = skipSpace(program);
@@ -23,4 +52,14 @@ function parseExpression(program) {
     }
 
     return parseApply(expr, program.slice(match[0].length))
-};
+}
+
+function parse(program) {
+    const result = parseExpression(program);
+
+    if (skipSpace(result.rest).length > 0) {
+        throw new SyntaxError(`Unexpected text after the program`);
+    }
+
+    return result.expr
+}
